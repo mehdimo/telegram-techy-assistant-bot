9af3b06c2f30f265aae80bd5a98cd5cbba9ecd45
Mehdi Mohammadi <mehdi.mka@gmail.com>
2024-10-01 17:16:00 -0700

    sent payload

--- teleg_bot.py
+++ teleg_bot.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from dotenv import load_dotenv
+from load_online import get_daily_articles
 
 load_dotenv()
 TOKEN = os.getenv("TELE_BOT_TOKEN")
@@ -8,9 +9,11 @@ chatId = os.getenv("CHANNEL_CHAT_ID")
 send_url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
 def send_post():
-    payload = "Welcome to <b> Tech News Just for Fun </b> channel."
-    requests.post(send_url, json={'chat_id': chatId, 'parse_mode': 'HTML',
-                                  'text': payload})
+    articles = get_daily_articles()
+    for href, hdr, txt in articles:
+        payload = f"<h3>{hdr}</h3><div>{txt}</div><a href='{href}'>{href}</a>"
+        requests.post(send_url, json={'chat_id': chatId, 'parse_mode': 'HTML',
+                                      'text': payload})
 
 if __name__ == "__main__":
     send_post()
